4612bada385c71888632f0d0e95370f0d9d0fe15
anna-saf <safonova.irage@yandex.ru>
2022-07-22 23:45:02 +0300

    Final

---
+++ Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree tree4 = new Tree("4", null);
+            Tree tree5 = new Tree("5", null);
+            Tree tree1 = new Tree("1", new List<Tree>() {tree5}.AsEnumerable());
+            Tree tree2 = new Tree("2", null);
+            Tree tree3 = new Tree("3", new List<Tree>(){ tree1,tree2}.AsEnumerable());
+
+            List<string> names = Tree.GetAllNames(tree3);
+            foreach(string name in names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.ReadLine();
+            
+        }
+
+    }
+}
---
+++ Task3/Tree.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task3
+{
+    public class Tree
+
+    {
+
+        public string Name { get; set; }
+
+        public IEnumerable<Tree> Children { get; set; }
+
+        public Tree(string name, IEnumerable<Tree> children)
+        {
+            Name = name;
+            Children = children;
+        }
+
+        public static List<string> GetAllNames(Tree tree)
+        { 
+            List<string> nameList = new List<string>();
+            RecurcyFuncGetNameInTrees(nameList, tree);
+            return nameList;
+            
+        }
+
+        private static void RecurcyFuncGetNameInTrees(List<string> nameList, Tree tree)
+        {
+            nameList.Add(tree.Name);
+            if (tree.Children != null)
+            {                
+                foreach (Tree child in tree.Children)
+                {                    
+                    RecurcyFuncGetNameInTrees(nameList,child);
+                }
+            } 
+        }
+
+    }
+    
+}
